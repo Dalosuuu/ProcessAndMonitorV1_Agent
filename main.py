9e9ba99aff8831eaf24d895e2e1390a1c3d5ac35
Dalosuuu <juankarlosazcuna@gmail.com>
2026-05-09 02:49:07 -0400

    adding basic argparse, making cycle sleep user controller

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from time import sleep
+import argparse
+from typing import cast
 
 
 from model.model import Proccess_Collector_Decider
 
 pc = Proccess_Collector_Decider()
 
+# Parser
+parser: argparse.ArgumentParser = argparse.ArgumentParser()
+_ = parser.add_argument(
+    "-c", "--cycle", help="set cycle loop speed used in sleep", default=5, type=int
+)
+
+args = parser.parse_args()
+
 while 1:
     # _ = os.system("clear")
 
+    cycleT = cast(int, args.cycle)
+
     events = pc.poll()
 
-    # print(events)
     for event in events:
         print(event, "\n")
     print("Total event last cycle: ", len(events))
-
-    sleep(3)
+    sleep(cycleT)
